2875c7ee451b9578ed541a62dd25fedb13b90491
nmadd <nate@fratmusic.com>
2017-01-02 15:56:45 -0500

    add CSS support

--- front/Entry.jsx
+++ front/Entry.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Routes} from './components';
+import './App.css';
 
 ReactDOM.render(
   <Routes />,
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,11 @@ module.exports = {
         query: {
           presets: ['es2015', 'react']
         }
-      }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
     ]
   },
   devtool: 'source-map',
