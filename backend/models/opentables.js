109eb08deb7452a3900ef614e6f6212a8f718084
Crazy Kiwi <Nasita-Haque>
2017-01-09 12:33:20 -0500

    Updated all models

--- backend/models/opentables.js
+++ backend/models/opentables.js
@@ -16,12 +16,13 @@ module.exports = function(sequelize, DataTypes){
        require:true,
        notNull:true
       }
-    }
+    },
     availability: {type: DataTypes.BOOLEAN, defaultValue: true}
   }, {
     classMethods: {
       associate: function(models) {
         OpenTable.belongsTo(models.Restaurant)
+        OpenTable.hasMany(models.Reservation)
       }
     }
   })
