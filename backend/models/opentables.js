47ec36221b941ea71eb6beb54b39006d4d8925ad
Crazy Kiwi <Nasita-Haque>
2017-01-09 12:53:05 -0500

    Models completed

--- backend/models/opentables.js
+++ backend/models/opentables.js
@@ -16,7 +16,6 @@ module.exports = function(sequelize, DataTypes){
     classMethods: {
       associate: function(models) {
         OpenTable.belongsTo(models.Restaurant)
-        OpenTable.hasMany(models.Reservation)
       }
     }
   })
