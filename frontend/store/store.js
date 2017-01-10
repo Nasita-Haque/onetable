86b8aedd5ed8d142ac84f48f3362f0e772710c74
Ambar Gutierrez <ambar.grz@gmaail.com>
2017-01-10 12:36:05 -0500

    fixing merge conflicts

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,11 +1,11 @@
 import {createStore} from "redux"
-import opentable from "../reducer/opentable-reducer.js"
+import reducer from "../reducer/opentable-reducer.js"
 import signup from "../reducer/signup-reducer.js"
 
 
 
 
-const store = createStore(signup, window.__REDUX_DEVTOOLS_EXTENSION__  && window.__REDUX_DEVTOOLS_EXTENSION__() );
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__  && window.__REDUX_DEVTOOLS_EXTENSION__() );
 
 
 export default store;
