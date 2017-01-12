7ea5821bc6852300fa4baede00e4c333a1c01bdd
ChaniceStl <chanicestlouis2020@gmail.com>
2017-01-11 19:12:39 -0500

    opentable component

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,8 +1,13 @@
-import {createStore} from "redux";
+import {createStore, applyMiddleware, compose} from "redux";
 import allReducers from '../reducer';
+import thunk from 'redux-thunk';
+//understands when you return a function 
+//thunk is necessary if doing async actions. 
 
 
-const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__  && window.__REDUX_DEVTOOLS_EXTENSION__() );
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(allReducers, composeEnhancers(applyMiddleware(thunk)
+));
 
 export default store;
