38b90f3214497125e684b432a08d9759dc9530fb
Crazy Kiwi <Nasita-Haque>
2017-01-13 11:00:51 -0500

    .

--- frontend/component/opentable/displayRestaurant.js
+++ frontend/component/opentable/displayRestaurant.js
@@ -5,9 +5,10 @@ import axios from 'axios'
 
 const DisplayRestaurant = React.createClass({
 	render(){
+
 		return(
-				<div>
-					<div id='siteContainer-displayRes'>
+				<div id='siteContainer-displayRes'>
+					<div id="secondContainer">
 
 					<div id='restaurantContainer-name'>
 						<div id="restaurant-name"><h1>{this.props.displayState.data ? this.props.displayState.data.name : 'loading...'}</h1></div> 
@@ -16,7 +17,10 @@ const DisplayRestaurant = React.createClass({
 						<br/>
 
 					<div id="restaurantContainer-location">
+						<div className="bold">Restaurant</div>
 						<div id='restaurant-address'>{this.props.displayState.data ? this.props.displayState.data.address : 'loading...'}</div> 
+						<br/>
+						<div className="bold">Address</div>
 						<div id='restaurant-city'>{this.props.displayState.data ? this.props.displayState.data.city : 'loading...'}</div> 
 						<div id='restaurant-zipcode'>{this.props.displayState.data ? this.props.displayState.data.zipcode : 'loading...'}</div> 
 					</div>	
@@ -24,8 +28,12 @@ const DisplayRestaurant = React.createClass({
 						<br/>
 
 					<div id='restaurantContainer-info'>
+						<div className="bold">Type</div>
 						<div id='restaurant-cuisine'>{this.props.displayState.data ? this.props.displayState.data.cuisine : 'loading...'}</div> 
+						<br/>
+						<div className="bold">Price</div>
 						<div id='restaurant-price'>{this.props.displayState.data ? this.props.displayState.data.price : 'loading...'}</div> 
+						
  					</div>
 
  					</div>
