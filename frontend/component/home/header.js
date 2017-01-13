8cbee2ead165edd12acd8705e4f18e5fef038c29
Crazy Kiwi <Nasita-Haque>
2017-01-12 20:54:35 -0500

    header styling complete

--- frontend/component/home/header.js
+++ frontend/component/home/header.js
@@ -5,8 +5,9 @@ const Header = React.createClass({
   render() {
     return (
       <div className='header'>
-        <h2>ONE TABLE</h2>
-        <h2>Hello Name!</h2>
+        <img className="logo" src="http://i.imgur.com/Dy7Esmv.gif"></img>
+        <div className="border"></div>
+        <h2 className='username'>Hello Name!</h2>
       </div>
     )
   }
